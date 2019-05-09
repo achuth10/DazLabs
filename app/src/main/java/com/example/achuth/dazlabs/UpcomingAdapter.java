9d94e1ff289bcc0587a7b7248f24363621347ae6
achuth10 <43288837+achuth10@users.noreply.github.com>
2019-05-09 11:59:41 +0530

    Bug fixes

--- app/src/main/java/com/example/achuth/dazlabs/UpcomingAdapter.java
+++ app/src/main/java/com/example/achuth/dazlabs/UpcomingAdapter.java
@@ -24,21 +24,22 @@ import java.util.ArrayList;
 
 public class UpcomingAdapter extends RecyclerView.Adapter <UpcomingAdapter.UpcomingHolder> {
     private Context context;
-    private ArrayList <Obj> objs;
-    UpcomingAdapter(Context context,ArrayList <Obj> objs)
-    {
-        this.context=context;
-        this.objs=objs;
+    private ArrayList<Obj> objs;
+
+    UpcomingAdapter(Context context, ArrayList<Obj> objs) {
+        this.context = context;
+        this.objs = objs;
     }
+
     @Override
-    public UpcomingHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(context).inflate(R.layout.upcominglayout,viewGroup,false);
+    public UpcomingHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.upcominglayout, viewGroup, false);
         return new UpcomingHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull UpcomingHolder upcomingHolder, int i) {
-        Obj obj= objs.get(i);
+        Obj obj = objs.get(i);
         upcomingHolder.setdetails(obj);
     }
 
@@ -48,29 +49,28 @@ public class UpcomingAdapter extends RecyclerView.Adapter <UpcomingAdapter.Upcom
     }
 
 
-
-    public class UpcomingHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        private TextView name,about;
-        private ImageButton whatsaap,phonebtn;
+    public class UpcomingHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView name, about;
+        private ImageButton whatsaap, phonebtn;
         private ImageView delete;
         private PackageManager pm;
         private final Context context;
+
         public UpcomingHolder(@NonNull View itemView) {
             super(itemView);
-            context=itemView.getContext();
-            pm=context.getPackageManager();
-            name=(TextView)itemView.findViewById(R.id.UpcomingName);
-            about=(TextView)itemView.findViewById(R.id.upcomingabout);
-            delete=(ImageView)itemView.findViewById(R.id.UpcomingDelete);
-            whatsaap=(ImageButton)itemView.findViewById(R.id.whatsaapbtn);
-            phonebtn=(ImageButton)itemView.findViewById(R.id.Phonebtn);
+            context = itemView.getContext();
+            pm = context.getPackageManager();
+            name = (TextView) itemView.findViewById(R.id.UpcomingName);
+            about = (TextView) itemView.findViewById(R.id.upcomingabout);
+            delete = (ImageView) itemView.findViewById(R.id.Upcomingedit);
+            whatsaap = (ImageButton) itemView.findViewById(R.id.whatsaapbtn);
+            phonebtn = (ImageButton) itemView.findViewById(R.id.Phonebtn);
             whatsaap.setOnClickListener(this);
             phonebtn.setOnClickListener(this);
             delete.setOnClickListener(this);
         }
 
-        public void setdetails(Obj obj)
-        {
+        public void setdetails(Obj obj) {
             name.setText(obj.getName());
             about.setText(obj.getAbout());
         }
@@ -78,52 +78,49 @@ public class UpcomingAdapter extends RecyclerView.Adapter <UpcomingAdapter.Upcom
         @Override
         public void onClick(View v) {
 
-            if(v.getId()==whatsaap.getId())
-            {
-                try{
-                    String message= "Hello";
-                    String phone ="+919999999999";
-                    String url = "https://api.whatsapp.com/send?phone="+ phone +"&text=" + URLEncoder.encode(message, "UTF-8");
-                    PackageInfo info=pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
+            if (v.getId() == whatsaap.getId()) {
+                try {
+                    String message = "Hello";
+                    String phone = "+919999999999";
+                    String url = "https://api.whatsapp.com/send?phone=" + phone + "&text=" + URLEncoder.encode(message, "UTF-8");
+                    PackageInfo info = pm.getPackageInfo("com.whatsapp", PackageManager.GET_META_DATA);
                     Intent sendIntent = new Intent();
                     sendIntent.setData(Uri.parse(url));
                     sendIntent.setPackage("com.whatsapp");
                     context.startActivity(sendIntent);
-                }
-                catch (PackageManager.NameNotFoundException e )
-                {
-                    Toast.makeText(context,"Whatsaap not installed",Toast.LENGTH_SHORT).show();
+                } catch (PackageManager.NameNotFoundException e) {
+                    Toast.makeText(context, "Whatsaap not installed", Toast.LENGTH_SHORT).show();
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
-            }
-            else if (v.getId()==phonebtn.getId())
-            {
+            } else if (v.getId() == phonebtn.getId()) {
                 Intent callIntent = new Intent(Intent.ACTION_DIAL);
-                callIntent.setData(Uri.parse("tel:"+"9999999999"));
+                callIntent.setData(Uri.parse("tel:" + "9999999999"));
                 context.startActivity(callIntent);
             }
-            else if(v.getId()==delete.getId())
-            {
-                AlertDialog.Builder builder=new AlertDialog.Builder(context);
-                builder.setTitle("Delete lead");
-                builder.setMessage("Are you sure you want to delete this lead?");
-                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        deleteItem(getAdapterPosition());
-                    }
-                });
-                builder.setNegativeButton("Cancel",null);
-                builder.setIcon(R.drawable.ic_warning_black_24dp);
-                AlertDialog alertDialog =builder.create();
-                alertDialog.show();
-            }
+//            else if(v.getId()==delete.getId())
+//            {
+//                AlertDialog.Builder builder=new AlertDialog.Builder(context);
+//                builder.setTitle("Delete lead");
+//                builder.setMessage("Are you sure you want to delete this lead?");
+//                builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        deleteItem(getAdapterPosition());
+//                    }
+//                });
+//                builder.setNegativeButton("Cancel",null);
+//                builder.setIcon(R.drawable.ic_warning_black_24dp);
+//                AlertDialog alertDialog =builder.create();
+//                alertDialog.show();
+//            }
+//        }
+//    }
+//    private void deleteItem(int adapterPosition) {
+//        objs.remove(adapterPosition);
+//        notifyItemRemoved(adapterPosition);
+//    }
+
         }
     }
-    private void deleteItem(int adapterPosition) {
-        objs.remove(adapterPosition);
-        notifyItemRemoved(adapterPosition);
-    }
-
 }
